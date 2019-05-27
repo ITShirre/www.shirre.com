3f7672fa44504d12f2fa32ceae2d86a4dc0e3834
IT Shirre <it@shirre.com>
2019-05-27 22:08:02 +0800

    move from old repositories

---
+++ static/custom.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+   $("#product_slick").slick();
+   $("#news_slick").slick({
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+   x=$('#navbar').on('activate.bs.scrollspy', function () {
+       var activeSection = $(this).find("li.active a").attr("href");
+
+       //if (activeSection == "#contact ")
+       //{
+        //  console.log(activeSection);
+       //}
+   });
+
+
+   $('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+setTimeout(function(){
+  $('#a_news').trigger('click');
+},2000);
+
+});
+
